7fbe4de754f5f38e627eb2908c780e33e44a0f23
Miguel Lara <mlarav1@unicartagena.edu.co>
2026-09-16 21:39:49 -0500

    feat(sudoku): agregar matriz 9x9

---
+++ src/SudokuValidator.java
@@ -0,0 +1,26 @@
+public class SudokuValidator {
+
+    public static void main(String[] args) {
+
+        int[][] sudoku = {
+            {5, 3, 4, 6, 7, 8, 9, 1, 2},
+            {6, 7, 2, 1, 9, 5, 3, 4, 8},
+            {1, 9, 8, 3, 4, 2, 5, 6, 7},
+            {8, 5, 9, 7, 6, 1, 4, 2, 3},
+            {4, 2, 6, 8, 5, 3, 7, 9, 1},
+            {7, 1, 3, 9, 2, 4, 8, 5, 6},
+            {9, 6, 1, 5, 3, 7, 2, 8, 4},
+            {2, 8, 7, 4, 1, 9, 6, 3, 5},
+            {3, 4, 5, 2, 8, 6, 1, 7, 9}
+        };
+
+        System.out.println("Tablero de Sudoku:");
+
+        for (int fila = 0; fila < 9; fila++) {
+            for (int columna = 0; columna < 9; columna++) {
+                System.out.print(sudoku[fila][columna] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
